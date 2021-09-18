5b09698b883a348f6444259e24c179cf448c3767
Steve Ernstberger <>
2021-09-18 10:18:34 -0400

    removed uneccessary boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,6 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import ProTip from './ProTip';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}.
-    </Typography>
-  );
-}
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 export default function App() {
   return (
@@ -24,8 +9,6 @@ export default function App() {
         <Typography variant="h4" component="h1" gutterBottom>
           Steve Ernstberger
         </Typography>
-        <ProTip />
-        <Copyright />
       </Box>
     </Container>
   );
