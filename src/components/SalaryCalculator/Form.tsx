9537a40fb7ca0597c850ec6029d45541c0371f8b
Steve Ernstberger <>
2021-09-20 18:44:39 -0400

    progress on form

--- src/components/SalaryCalculator/Form.tsx
+++ src/components/SalaryCalculator/Form.tsx
@@ -1,98 +1,44 @@
 import React, { useState } from "react";
-import { Grid, TextField, Button } from "@mui/material";
+import { Grid, Button } from "@mui/material";
 import { Formik, Form, FormikProps } from "formik";
 import * as Yup from "yup";
+import Input from "../Form/Input";
 
-interface ISignUpForm {
-  fullName: string;
-  password: string;
-  confirmPassword: string;
-  email: string;
+interface IncomeCalculatorFormProps {
+  startingSalary: number;
+  growthRate: number;
+  years: number;
 }
-
-interface IFormStatus {
-  message: string;
-  type: string;
-}
-
-interface IFormStatusProps {
-  [key: string]: IFormStatus;
-}
-
-const formStatusProps: IFormStatusProps = {
-  success: {
-    message: "Signed up successfully.",
-    type: "success",
-  },
-  duplicate: {
-    message: "Email-id already exist. Please use different email-id.",
-    type: "error",
-  },
-  error: {
-    message: "Something went wrong. Please try again.",
-    type: "error",
-  },
-};
 
 const SignUp: React.FunctionComponent = () => {
-  const [formStatus, setFormStatus] = useState<IFormStatus>({
-    message: "",
-    type: "",
-  });
-
-  const createNewUser = async (data: ISignUpForm, resetForm: Function) => {
-    // try {
-    //   // API call integration will be here. Handle success / error response accordingly.
-    //   if (data) {
-    //     setFormStatus(formStatusProps.success);
-    //     resetForm({});
-    //   }
-    // } catch (error) {
-    //   // @ts-ignore
-    //   const response = error.response;
-    //   if (response.data === "user already exist" && response.status === 400) {
-    //     setFormStatus(formStatusProps.duplicate);
-    //   } else {
-    //     setFormStatus(formStatusProps.error);
-    //   }
-    // } finally {
-    //   setDisplayFormStatus(true);
-    // }
+  const createNewUser = async (
+    data: IncomeCalculatorFormProps,
+    resetForm: Function
+  ) => {
+    // calculate
   };
 
   return (
     <div>
       <Formik
         initialValues={{
-          fullName: "",
-          password: "",
-          confirmPassword: "",
-          email: "",
+          startingSalary: 20000,
+          growthRate: 5,
+          years: 20,
         }}
-        onSubmit={(values: ISignUpForm, actions) => {
+        onSubmit={(values: IncomeCalculatorFormProps, actions) => {
           createNewUser(values, actions.resetForm);
           setTimeout(() => {
             actions.setSubmitting(false);
           }, 500);
         }}
         validationSchema={Yup.object().shape({
-          email: Yup.string().email().required("Enter valid email-id"),
-          fullName: Yup.string().required("Please enter full name"),
-          password: Yup.string()
-            .matches(
-              /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()]).{8,20}\S$/
-            )
-            .required(
-              "Please valid password. One uppercase, one lowercase, one special character and no spaces"
-            ),
-          confirmPassword: Yup.string()
-            .required("Required")
-            .test("password-match", "Password musth match", function (value) {
-              return this.parent.password === value;
-            }),
+          startingSalary: Yup.number().required(),
+          growthRate: Yup.number().required("Goooooooooooo"),
+          years: Yup.number().required(),
         })}
       >
-        {(props: FormikProps<ISignUpForm>) => {
+        {(props: FormikProps<IncomeCalculatorFormProps>) => {
           const {
             values,
             touched,
@@ -103,88 +49,28 @@ const SignUp: React.FunctionComponent = () => {
           } = props;
           return (
             <Form>
-              <h1>Sign up</h1>
-              <Grid container>
-                <Grid item lg={10} md={10} sm={10} xs={10}>
-                  <TextField
-                    name="fullName"
-                    id="fullName"
-                    label="Full Name"
-                    value={values.fullName}
-                    type="text"
-                    helperText={
-                      errors.fullName && touched.fullName
-                        ? errors.fullName
-                        : "Enter your full name."
-                    }
-                    error={errors.fullName && touched.fullName ? true : false}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
+              <Grid container spacing={3}>
+                <Grid item md={4} xs={12}>
+                  <Input
+                    name="startingSalary"
+                    label="Starting salary"
+                    helperText="Fooooooooo"
                   />
                 </Grid>
-                <Grid item lg={10} md={10} sm={10} xs={10}>
-                  <TextField
-                    name="password"
-                    id="password"
-                    label="Password"
-                    value={values.password}
-                    type="password"
-                    helperText={
-                      errors.password && touched.password
-                        ? "Please valid password. One uppercase, one lowercase, one special character and no spaces"
-                        : "One uppercase, one lowercase, one special character and no spaces"
-                    }
-                    error={errors.password && touched.password ? true : false}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                  />
+                <Grid item md={4} xs={12}>
+                  <Input name="growthRate" label="Growth rate" />
                 </Grid>
-                <Grid item lg={10} md={10} sm={10} xs={10}>
-                  <TextField
-                    name="confirmPassword"
-                    id="confirmPassword"
-                    label="Confirm password"
-                    value={values.confirmPassword}
-                    type="password"
-                    helperText={
-                      errors.confirmPassword && touched.confirmPassword
-                        ? errors.confirmPassword
-                        : "Re-enter password to confirm"
-                    }
-                    error={
-                      errors.confirmPassword && touched.confirmPassword
-                        ? true
-                        : false
-                    }
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                  />
+                <Grid item md={4} xs={12}>
+                  <Input name="years" label="Years" />
                 </Grid>
-                <Grid item lg={10} md={10} sm={10} xs={10}>
-                  <TextField
-                    name="email"
-                    id="email"
-                    label="Email-id"
-                    value={values.email}
-                    type="email"
-                    helperText={
-                      errors.email && touched.email
-                        ? errors.email
-                        : "Enter email-id"
-                    }
-                    error={errors.email && touched.email ? true : false}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                  />
-                </Grid>
-                <Grid item lg={10} md={10} sm={10} xs={10}>
+                <Grid item md={4} xs={12}>
                   <Button
                     type="submit"
                     variant="contained"
                     color="secondary"
                     disabled={isSubmitting}
                   >
-                    Submit
+                    Calculate
                   </Button>
                 </Grid>
               </Grid>
--- src/components/SalaryCalculator/index.tsx
+++ src/components/SalaryCalculator/index.tsx
@@ -98,30 +98,7 @@ const SalaryCalculator = () => {
       />
 
       {/* <form onSubmit={formik.handleSubmit}>
-        <TextField
-          fullWidth
-          id="email"
-          name="email"
-          label="Email"
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          error={formik.touched.email && Boolean(formik.errors.email)}
-          helperText={formik.touched.email && formik.errors.email}
-        />
-        <TextField
-          fullWidth
-          id="password"
-          name="password"
-          label="Password"
-          type="password"
-          value={formik.values.password}
-          onChange={formik.handleChange}
-          error={formik.touched.password && Boolean(formik.errors.password)}
-          helperText={formik.touched.password && formik.errors.password}
-        />
-        <Button color="primary" variant="contained" fullWidth type="submit">
-          Submit
-        </Button>
+        
       </form> */}
       <Form />
 
